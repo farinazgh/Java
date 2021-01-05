1532fbc4d15599d96ea463e3ae0a8671e3e3af63
Farinaz <farinaz.ghasemi@gmail.com>
2021-01-05 22:29:46 +0100

    anonymous classes vs lambda expressions

--- src/main/java/com/modernjava/behaviorparameterization/Main.java
+++ src/main/java/com/modernjava/behaviorparameterization/Main.java
@@ -36,5 +36,21 @@ public class Main {
         System.out.println(green);
         System.out.println(heavy);
         System.out.println(readAndHeavy);
+        List<Apple> red = filterApples(inventory, (Apple apple) -> apple.getColor().equals("red"));
+        System.out.println(red);
+        System.out.println(inventory);
+
+        inventory.sort((Apple a1, Apple a2) -> a2.getWeight() - a1.getWeight());
+        System.out.println(inventory);
+    }
+
+    public static <T> List<T> filter(List<T> list, Predicate<T> p) {
+        List<T> result = new ArrayList<>();
+        for (T e : list) {
+            if (p.test(e)) {
+                result.add(e);
+            }
+        }
+        return result;
     }
 }
---
+++ src/main/java/com/modernjava/behaviorparameterization/Predicate.java
@@ -0,0 +1,5 @@
+package com.modernjava.behaviorparameterization;
+
+public interface Predicate<T> {
+    boolean test(T t);
+}
