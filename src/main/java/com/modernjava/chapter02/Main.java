df7268ba55019296b29fe34f7734fff676e27312
Farinaz Ghasemi <farinaz.ghasemi@gmail.com>
2021-02-22 12:05:07 +0330

    generics

--- src/main/java/com/modernjava/chapter02/Main.java
+++ src/main/java/com/modernjava/chapter02/Main.java
@@ -1,6 +1,7 @@
 package com.modernjava.chapter02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -12,5 +13,21 @@ public class Main {
             }
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        List<Apple> apples = Arrays.asList(
+                new Apple(80, Color.GREEN),
+                new Apple(155, Color.RED),
+                new Apple(120, Color.RED));
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        List<Apple> redApples =
+                filter(apples, (Apple apple) -> Color.RED.equals(apple.getColor()));
+        List<Integer> evenNumbers =
+                filter(numbers, (Integer i) -> i % 2 == 0);
+
+        System.out.println(redApples);
+        System.out.println(evenNumbers);
     }
 }
