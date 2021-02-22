b79c5d3bc1a0a51901fb333e7f905e0878e12b88
Farinaz Ghasemi <farinaz.ghasemi@gmail.com>
2021-02-22 12:02:24 +0330

    generics

---
+++ src/main/java/com/modernjava/chapter02/Main.java
@@ -0,0 +1,16 @@
+package com.modernjava.chapter02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static <T> List<T> filter(List<T> list, Predicate<T> p) {
+        List<T> result = new ArrayList<>();
+        for (T e : list) {
+            if (p.test(e)) {
+                result.add(e);
+            }
+        }
+        return result;
+    }
+}
---
+++ src/main/java/com/modernjava/chapter02/Predicate.java
@@ -0,0 +1,5 @@
+package com.modernjava.chapter02;
+
+public interface Predicate<T> {
+    boolean test(T t);
+}
